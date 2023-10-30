fed0c29ba168d5ac38299b6a90242229c13f246e
FLUKE-CC15 <wacharin1504@gmail.com>
2023-10-30 16:29:16 +0700

    add/setup folder and file

---
+++ src/router/Router.jsx
@@ -0,0 +1,22 @@
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from '../components/layout/Layout'
+import Home from '../components/home/Home'
+import UserProfile from '../components/user/UserProfile'
+import Admin from '../components/admin/AdminProfile'
+
+const router = createBrowserRouter(
+    [{
+        path: '/',
+        element: <Layout />,
+        children: [
+            { path: '/', element: <Home /> },
+            { path: '/profile', element: <UserProfile /> },
+            { path: '/Admin', element: <Admin /> },
+
+        ]
+    }
+    ]
+)
+export default function Router() {
+    return <RouterProvider router={router}> </RouterProvider>
+}
