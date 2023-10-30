abb791f1a10f70f7a580fcf691f3d29bbe4d4101
mist3rW <wannarak.nutt@gmail.com>
2023-10-30 22:24:22 +0700

    page-single-item-page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,19 +1,21 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "../components/layout/Layout";
-import Home from "../components/home/Home";
-import UserProfile from "../components/user/UserProfile";
-import Admin from "../components/admin/AdminProfile";
-import ProductListingPage from "../components/user/ProductListingPage";
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from '../components/layout/Layout';
+import Home from '../components/home/Home';
+import UserProfile from '../components/user/UserProfile';
+import Admin from '../components/admin/AdminProfile';
+import ProductListingPage from '../components/user/ProductListingPage';
+import SingleProductPage from '../components/user/SIngleProductPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/profile", element: <UserProfile /> },
-      { path: "/Admin", element: <Admin /> },
-      { path: "/product-listing", element: <ProductListingPage /> },
+      { path: '/', element: <Home /> },
+      { path: '/profile', element: <UserProfile /> },
+      { path: '/Admin', element: <Admin /> },
+      { path: '/product-listing', element: <ProductListingPage /> },
+      { path: '/single-product', element: <SingleProductPage /> },
     ],
   },
 ]);
