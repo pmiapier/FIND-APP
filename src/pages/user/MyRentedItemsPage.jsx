e9ea9606dc8cb6e6b7322edfc74465ef03129c20
PlumeSC <supawit.chukiat@gmail.com>
2023-11-08 11:14:47 +0700

    myRentalAndRented90percent

--- src/pages/user/MyRentedItemsPage.jsx
+++ src/pages/user/MyRentedItemsPage.jsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import MyRentedItemCard from '../../components/cards/MyRentedItemCard';
+import axios from '../../config/axios';
 
 export default function MyRentedItemsPage() {
+  const [items,setItems] = useState([])
+  useEffect(()=>{
+    getMyRentedItem()
+  },[])
+  const getMyRentedItem = async()=>{
+    const res = await axios.get(`/item/myRentedItem`)
+    console.log(res.data[0]);
+    setItems(res.data)
+  }
   return (
     <div className="bg-primaryBackground px-20 py-5 ">
       <div className="text-[30px]">My Rented Items การเช่าของฉัน</div>
       <div className="flex flex-col gap-5">
-        <MyRentedItemCard />
-        <MyRentedItemCard />
-        <MyRentedItemCard />
+        {items?items.map((item,index)=>{
+          return (<MyRentedItemCard key={index} data={item}/>)
+        }):null}
+
       </div>
     </div>
   );
