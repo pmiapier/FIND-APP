24ce86f74810bb95a3ba502963802b423e1feb5e
mist3rW <wannarak.nutt@gmail.com>
2023-11-08 19:41:36 +0700

    finish image and image gallery, no validation

--- src/components/items/UserAddProduct.jsx
+++ src/components/items/UserAddProduct.jsx
@@ -1,167 +1,11 @@
-// import ReactQuill, { Quill } from 'react-quill';
-// import 'react-quill/dist/quill.snow.css';
-// import ImageResize from 'quill-image-resize-module-react';
-// import ImageCompress from 'quill-image-compress';
-// import Button from '../buttons/Button';
-// import { useState } from 'react';
-// import axios from 'axios';
-
-// Quill.register('modules/imageResize', ImageResize);
-// Quill.register('modules/imageCompress', ImageCompress);
-
-// const modules = {
-//   imageResize: {
-//     parchment: Quill.import('parchment'),
-//     modules: ['Resize', 'DisplaySize'],
-//     background: 'black',
-//     padding: '100px'
-//   },
-//   imageCompress: {
-//     quality: 1.0,
-//     maxWidth: 1000,
-//     maxHeight: 1000,
-//     imageType: 'image/jpeg',
-//     debug: false,
-//     suppressErrorLogging: false,
-//     insertIntoEditor: undefined
-//   },
-//   toolbar: [
-//     [{ header: [1, 2, 3, 4, 5, 6, false] }],
-//     [{ font: [] }],
-//     [{ size: [] }],
-//     ['bold', 'italic', 'underline', 'strike', 'blackquote'],
-//     [{ align: [] }],
-//     [{ color: [] }, { background: [] }],
-//     [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-//     ['link', 'image', 'video'],
-//     [{ direction: 'rtl' }],
-//     ['clean']
-//   ]
-// };
-
-// const UserAddProduct = ({ category, onCloseModal }) => {
-//   const [input, setInput] = useState({
-//     itemName: '',
-//     itemCategory: 'Vehicles',
-//     itemDescription: '',
-//     itemPrice: ''
-//   });
-//   const [files, setFiles] = useState([]);
-
-//   const handleInput = (e) => {
-//     if (e.target.files) {
-//       return setFiles([...e.target.files]);
-//     }
-
-//     setInput({ ...input, [e.target.name]: e.target.value });
-//   };
-
-//   const handleItemDescription = (value) => {
-//     setInput({ ...input, itemDescription: value });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const formdata = new FormData();
-//     for (const key in input) {
-//       formdata.append(key, input[key]);
-//     }
-//     files.forEach((item, index) => {
-//       formdata.append(`file[${index}]`, item);
-//     });
-
-//     const headers = {
-//       Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`,
-//       'Content-Type': 'multipart/form-data'
-//     };
-//     const res = await axios.post(`http://localhost:8000/user/postItem`, formdata, { headers });
-//     if (res.data.message) {
-//       onCloseModal();
-//     }
-//   };
-
-//   return (
-//     <form
-//       encType="multipart/form-data"
-//       onSubmit={handleSubmit}
-//       className="flex flex-col gap-5 px-12 pt-5 pb-12 bg-white rounded-lg
-//     shadow-lg"
-//     >
-//       <div className="text-[30px]">ข้อมูลสินค้่า</div>
-//       <div className="grid grid-cols-9 gap-y-10 pl-1">
-//         <div className="col-span-1">
-//           <span className="text-red-600">*</span>ภาพสินค้า
-//         </div>
-//         <input type="file" multiple className="col-span-8" name="itemFile" onChange={handleInput}></input>
-
-//         <div className="col-span-1">
-//           <span className="text-red-600">*</span>ชื่อสินค้่า
-//         </div>
-//         <input
-//           className="col-span-8 pl-1 rounded-sm border-gray-200 border-2 py-2"
-//           type="text"
-//           placeholder="ชื่อสินค้า"
-//           name="itemName"
-//           onChange={handleInput}
-//         />
-
-//         <div className="col-span-1">
-//           <span className="text-red-600">*</span>เลือกหมวดหมู่
-//         </div>
-//         <select
-//           className="col-span-8 pl-1 rounded-sm border-gray-200 border-2 py-2"
-//           name="itemCategory"
-//           onChange={handleInput}
-//         >
-//           {category.map((item, index) => {
-//             return (
-//               <option key={index} value={item}>
-//                 {item}
-//               </option>
-//             );
-//           })}
-//         </select>
-
-//         <div className="col-span-1">
-//           <span className="text-red-600">*</span>รายละเอียดสินค้า
-//         </div>
-//         {/* <textarea className="outline outline-1 col-span-8 pl-1" cols="30" rows="4"></textarea> */}
-//         <div className="col-span-8">
-//           <ReactQuill
-//             theme="snow"
-//             placeholder="Content..."
-//             name="itemDescription"
-//             onChange={handleItemDescription}
-//             modules={modules}
-//             // value={input.itemDescription}
-//           />
-//         </div>
-
-//         <div className="col-span-1">
-//           <span className="text-red-600">*</span>รายเช่าต่อวัน
-//         </div>
-//         <input
-//           className="appearance-none pl-1 rounded-sm border-gray-200 border-2 py-2"
-//           type="number"
-//           name="itemPrice"
-//           onChange={handleInput}
-//         />
-//       </div>
-//       <div className="flex gap-5">
-//         <Button text="ยืนยัน" className="bg-blue-500" />
-//         <Button event={onCloseModal} text="ยกเลิก" className="bg-red-500" />
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default UserAddProduct;
-
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import ImageResize from 'quill-image-resize-module-react';
 import ImageCompress from 'quill-image-compress';
 import Button from '../buttons/Button';
+import InputField from '../inputs/InputField';
+import Joi from 'joi';
+import { toast } from 'react-toastify';
 
 import { useState } from 'react';
 import axios from 'axios';
@@ -189,7 +33,7 @@ const modules = {
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ font: [] }],
     [{ size: [] }],
-    ['bold', 'italic', 'underline', 'strike', 'blackquote'],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
     [{ align: [] }],
     [{ color: [] }, { background: [] }],
     [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
@@ -198,6 +42,14 @@ const modules = {
     ['clean']
   ]
 };
+
+const schema = Joi.object({
+  itemName: Joi.string().max(150).required().label('ชื่อสินค้า'),
+  itemCategory: Joi.string().required().label('หมวดหมู่'),
+  itemDescription: Joi.string().required().label('รายละเอียดสินค้า'),
+  itemPrice: Joi.number().required().label('ราคาเช่าต่อวัน'),
+  itemFile: Joi.array().min(1).max(4).required().label('ภาพสินค้า')
+});
 
 const UserAddProduct = ({ category, onCloseModal }) => {
   const [input, setInput] = useState({
@@ -208,14 +60,27 @@ const UserAddProduct = ({ category, onCloseModal }) => {
   });
 
   const [files, setFiles] = useState([]);
+  const [imagePreview, setImagePreview] = useState(null);
 
   const handleInput = (e) => {
     if (e.target.files) {
-      return setFiles([...e.target.files]);
+      setFiles([...files, ...e.target.files]);
+
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setImagePreview(event.target.result);
+      };
+    } else {
+      if (imagePreview) {
+        setImagePreview(null);
+      }
     }
-
-
     setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleRemoveImage = () => {
+    setFiles([]);
+    setImagePreview(null);
   };
 
   const handleItemDescription = (value) => {
@@ -223,8 +88,8 @@ const UserAddProduct = ({ category, onCloseModal }) => {
   };
 
   const handleSubmit = async (e) => {
-
     e.preventDefault();
+
     const formdata = new FormData();
     for (const key in input) {
       formdata.append(key, input[key]);
@@ -240,6 +105,9 @@ const UserAddProduct = ({ category, onCloseModal }) => {
     const res = await axios.post(`/user/postItem`, formdata, { headers });
     console.log(res);
     if (res.data.message === `post done`) {
+      toast.success('เพิ่มสินค้าสำเร็จ', {
+        position: toast.POSITION.TOP_CENTER
+      });
       onCloseModal();
     }
   };
@@ -251,67 +119,143 @@ const UserAddProduct = ({ category, onCloseModal }) => {
       className="flex flex-col gap-5 px-12 pt-5 pb-12 bg-white rounded-lg
     shadow-lg"
     >
-      <div className="text-[30px]">ข้อมูลสินค้่า</div>
-      <div className="grid grid-cols-9 gap-y-10 pl-1">
-        <div className="col-span-1">
-          <span className="text-red-600">*</span>ภาพสินค้า
+      <div className="flex justify-between items-center">
+        <div className="text-[30px]">ข้อมูลสินค้า</div>
+        <div className="text-xxl bg-red-500 px-2 text-white rounded-full cursor-pointer" onClick={onCloseModal}>
+          X
         </div>
-        <input type="file" multiple className="col-span-8" name="itemFile" onChange={handleInput}></input>
-
-        <div className="col-span-1">
-          <span className="text-red-600">*</span>ชื่อสินค้่า
+      </div>
+      <div className="flex flex-col space-y-4">
+        {/* Product Image */}
+        <div className="flex flex-row ">
+          <div className="basis-36">
+            <span className="text-red-600">*</span>ภาพสินค้า
+          </div>
+          <div className="basis-full">
+            <div className="relative flex flex-row gap-2">
+              {files.map((file, index) => (
+                <div key={index} className="relative border border-dotted rounded-md p-1">
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt={`Selected Image ${index}`}
+                    style={{ maxWidth: '80px', maxHeight: '80px' }}
+                  />
+                  <div
+                    className="text-white bg-red-500 text-[8px] rounded-full px-2 py-1 font-bold absolute top-[-5px] right-[-5px] cursor-pointer"
+                    onClick={() => {
+                      const updatedFiles = [...files];
+                      updatedFiles.splice(index, 1);
+                      setFiles(updatedFiles);
+                    }}
+                  >
+                    X
+                  </div>
+                </div>
+              ))}
+              {files.length < 4 && (
+                <div
+                  className="border border-dashed relative flex items-center"
+                  style={{ width: '80px', maxWidth: '80px', height: '80px', maxHeight: '80px' }}
+                >
+                  {imagePreview && (
+                    <div className="w-full h-full">
+                      <img src={imagePreview} alt="Selected Image" style={{ maxWidth: '80px', maxHeight: '80px' }} />
+                      <div className="absolute top-0 right-0 text-red-500 font-bold" onClick={handleRemoveImage}>
+                        x
+                      </div>
+                    </div>
+                  )}
+                  <label htmlFor="itemFile" className="custom-file-input" style={{ cursor: 'pointer' }}>
+                    <i className="file-upload-icon flex items-center justify-center">
+                      <svg viewBox="0 0 23 21" xmlns="http://www.w3.org/2000/svg" className="h-[23px] w-[23px]">
+                        <path d="M18.5 0A1.5 1.5 0 0120 1.5V12c-.49-.07-1.01-.07-1.5 0V1.5H2v12.65l3.395-3.408a.75.75 0 01.958-.087l.104.087L7.89 12.18l3.687-5.21a.75.75 0 01.96-.086l.103.087 3.391 3.405c.81.813.433 2.28-.398 3.07A5.235 5.235 0 0014.053 18H2a1.5 1.5 0 01-1.5-1.5v-15A1.5 1.5 0 012 0h16.5z"></path>
+                        <path d="M6.5 4.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM18.5 14.25a.75.75 0 011.5 0v2.25h2.25a.75.75 0 010 1.5H20v2.25a.75.75 0 01-1.5 0V18h-2.25a.75.75 0 010-1.5h2.25v-2.25z"></path>
+                      </svg>
+                    </i>
+                    <div className="text-xs text-center">
+                      เพิ่มรูปภาพ <span>({files.length}/4)</span>
+                    </div>
+                  </label>
+                  <input
+                    type="file"
+                    id="itemFile"
+                    name="itemFile"
+                    onChange={handleInput}
+                    multiple
+                    style={{ display: 'none' }}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-        <input
-          className="col-span-8 pl-1 rounded-sm border-gray-200 border-2 py-2"
-          type="text"
-          placeholder="ชื่อสินค้า"
-          name="itemName"
-          onChange={handleInput}
-        />
-
-        <div className="col-span-1">
-          <span className="text-red-600">*</span>เลือกหมวดหมู่
+        {/* Product Title */}
+        <div className="flex ">
+          <div className="basis-36">
+            <span className="text-red-600">*</span>ชื่อสินค้า
+          </div>
+          <div className="basis-full">
+            <InputField
+              placeholder="Product Title"
+              name="itemName"
+              onChange={handleInput}
+              limitText="150"
+              value={input.itemName}
+              hasError=""
+            />
+          </div>
         </div>
-        <select
-          className="col-span-8 pl-1 rounded-sm border-gray-200 border-2 py-2"
-          name="itemCategory"
-          onChange={handleInput}
-        >
-          {category.map((item, index) => {
-
-            return (
-              <option key={index} value={item}>
-                {item}
-              </option>
-            );
-
-          })}
-        </select>
-
-        <div className="col-span-1">
-          <span className="text-red-600">*</span>รายละเอียดสินค้า
+        {/* Category */}
+        <div className="flex">
+          <div className="basis-36">
+            <span className="text-red-600">*</span>เลือกหมวดหมู่
+          </div>
+          <select
+            className="basis-full pl-1 rounded-md border-gray-200 border-2 py-2"
+            name="itemCategory"
+            onChange={handleInput}
+          >
+            {category.map((item, index) => {
+              return (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              );
+            })}
+          </select>
         </div>
-        {/* <textarea className="outline outline-1 col-span-8 pl-1" cols="30" rows="4"></textarea> */}
-        <div className="col-span-8">
-          <ReactQuill
-            theme="snow"
-            placeholder="Content..."
-            name="itemDescription"
-            onChange={handleItemDescription}
-            modules={modules}
-            // value={input.itemDescription}
-          />
+        {/* Product Description */}
+        <div className="flex">
+          <div className="basis-36">
+            <span className="text-red-600">*</span>รายละเอียดสินค้า
+          </div>
+          <div className="basis-full">
+            <ReactQuill
+              theme="snow"
+              placeholder="Content..."
+              name="itemDescription"
+              onChange={handleItemDescription}
+              modules={modules}
+              // value={input.itemDescription}
+            />
+          </div>
         </div>
-
-        <div className="col-span-1">
-          <span className="text-red-600">*</span>รายเช่าต่อวัน
+        {/* Product Price */}
+        <div className="flex">
+          <div className="basis-36">
+            <span className="text-red-600">*</span>ราคาเช่าต่อวัน
+          </div>
+          <div className="basis-full">
+            <InputField
+              type="number"
+              placeholder="Product Price"
+              name="itemPrice"
+              onChange={handleInput}
+              value={input.itemPrice}
+              hasError=""
+            />
+          </div>
         </div>
-        <input
-          className="appearance-none pl-1 rounded-sm border-gray-200 border-2 py-2"
-          type="number"
-          name="itemPrice"
-          onChange={handleInput}
-        />
       </div>
       <div className="flex gap-5">
         <Button text="ยืนยัน" className="bg-blue-500" />
