e38766f5f1a290cba84bfefc2b432e0c67cc5cde
mist3rW <wannarak.nutt@gmail.com>
2023-11-08 21:15:04 +0700

    before updating develop to my product

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -5,7 +5,7 @@ import axios from '../config/axios';
 
 export default function ProductContextProvider({ children }) {
   const [items, setItems] = useState([]); // [{}
-  const [category,setCategory] = useState([])
+  const [category, setCategory] = useState([]);
 
   const getItems = () => {
     axios.get('/item').then((response) => {
@@ -13,16 +13,16 @@ export default function ProductContextProvider({ children }) {
     });
   };
 
-  const getCategory = () =>{
-    axios.get(`/item/getCategories`).then(response=>{
-      setCategory(response.data)
-    })
-  }
+  const getCategory = () => {
+    axios.get(`/item/getCategories`).then((response) => {
+      setCategory(response.data);
+    });
+  };
 
   useEffect(() => {
     getItems();
-    getCategory()
+    getCategory();
   }, []);
 
-  return <ProductContext.Provider value={{ items, setItems, getItems,category }}>{children}</ProductContext.Provider>;
+  return <ProductContext.Provider value={{ items, setItems, getItems, category }}>{children}</ProductContext.Provider>;
 }
--- src/pages/user/MyProductPage.jsx
+++ src/pages/user/MyProductPage.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import MyItemCard from '../../components/cards/MyItemCard';
 import { useModal } from '../../hooks/useModal';
 import AddItemModal from '../../modal/AddItemModal';
+import axios from '../../config/axios';
+import { useAuth } from '../../hooks/useAuth';
 
 export default function MyProductPage() {
+  const [myProduct, setMyProduct] = useState();
+  const { authUser } = useAuth();
   const { onOpenModal } = useModal();
+  console.log('authUser', authUser);
+  console.log('my product', myProduct);
+  useEffect(() => {
+    const getMyProductData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/user/my-product', {
+          userId: authUser.id
+        });
+        console.log('response.data', response.data);
+        setMyProduct(response.data);
+      } catch (error) {
+        console.error('Error fetching my product data: ', error);
+      }
+    };
+    getMyProductData();
+  }, []);
   return (
     <div className="flex flex-col bg-primaryBackground px-10 py-10 gap-10">
       <div className="text-lg">You have 8 items in total</div>
