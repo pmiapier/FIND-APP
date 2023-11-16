58ee2f8743f15cb2d0bd8f0076b9449652498c5e
WacharinCh <wacharin1504@gmail.com>
2023-11-16 22:42:58 +0700

    fix online user

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import Router from './router/Router';
 import { ToastContainer } from 'react-toastify';
 import { socket } from './pages/chat/confic/socket';
 import { useAuth } from './hooks/useAuth';
+import { useEffect } from 'react';
+
 export default function App() {
+  const { authUser } = useAuth();
+  useEffect(() => {
+    if (authUser) {
+      socket.auth = { authUser }
+      socket.connect()
+    }
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
+
 
   return (
     <>
