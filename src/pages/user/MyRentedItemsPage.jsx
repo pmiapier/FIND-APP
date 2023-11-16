dbff19e474b8e051c0e1ec6ef443b9fd804686b5
pmiapier <pmiapier@proton.me>
2023-11-16 19:56:24 +0700

    fixed the rental and rented card to be in the center

--- src/pages/user/MyRentedItemsPage.jsx
+++ src/pages/user/MyRentedItemsPage.jsx
@@ -3,22 +3,23 @@ import MyRentedItemCard from '../../components/cards/MyRentedItemCard';
 import axios from '../../config/axios';
 
 export default function MyRentedItemsPage() {
-  const [items,setItems] = useState([])
-  useEffect(()=>{
-    getMyRentedItem()
-  },[])
-  const getMyRentedItem = async()=>{
-    const res = await axios.get(`/item/myRentedItem`)
-    setItems(res.data)
-  }
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    getMyRentedItem();
+  }, []);
+  const getMyRentedItem = async () => {
+    const res = await axios.get(`/item/myRentedItem`);
+    setItems(res.data);
+  };
   return (
     <div className="bg-primaryBackground px-20 py-5 ">
-      <div className="text-[30px]">My Rented Items</div>
-      <div className="flex flex-col gap-5 z-0">
-        {items?items.map((item,index)=>{
-          return (<MyRentedItemCard key={index} data={item}/>)
-        }):null}
-
+      <div className="text-[30px] font-bold text-center">MY RENTED ITEMS</div>
+      <div className="flex flex-col gap-5 z-0 items-center">
+        {items
+          ? items.map((item, index) => {
+              return <MyRentedItemCard key={index} data={item} />;
+            })
+          : null}
       </div>
     </div>
   );
